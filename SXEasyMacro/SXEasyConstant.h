cd68b9c77226cac5ee3983e18e02470c64df0d2e
DongShangXian <dantesx2012@gmail.com>
2015-10-17 21:26:50 +0800

    创建了SXEasyConstant 用来存放一些常用数值，不至于项目中乱七八糟的数字

---
+++ SXEasyMacro/SXEasyConstant.h
@@ -0,0 +1,12 @@
+//
+//  SXEasyConstant.h
+//  SXEasyMacroDemo
+//
+//  Created by dongshangxian on 15/10/17.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#ifndef SXEasyConstant_h
+#define SXEasyConstant_h
+
+#endif /* SXEasyConstant_h */
